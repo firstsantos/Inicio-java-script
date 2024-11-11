e38c2f4fca06f2422318979c5dc235a208a2865c
Artur Moreira dos Santos <arturmoreira56dossantos@gmail.com>
2024-11-11 17:45:43 -0300

    Condições e suas variavies

---
+++ Curso JS/aula11/ex009.js
@@ -0,0 +1,9 @@
+var país = 'Frances'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log(`Você é Brasileiro`)
+} else {
+    console.log(`Você é Estrangeiro!`)
+} if (país == 'França') {
+    console.log('E Frances')
+}
