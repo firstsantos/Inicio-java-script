6c16a18434611680f4f2e0e1e28b2bd213de272a
Artur Moreira dos Santos <arturmoreira56dossantos@gmail.com>
2024-11-13 17:31:19 -0300

    Update total

---
+++ Curso JS/Execercícios/script01/script.js
@@ -0,0 +1,43 @@
+function verificar() {
+    var data = new Date();
+    var ano = data.getFullYear();
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'image/Fotobb M.jpg');
+            } else if (idade < 21) {
+                img.setAttribute('src', 'image/Fotojovem M.jpg');
+            } else if (idade < 50) {
+                img.setAttribute('src', 'image/Fotoadulto M.jpg');
+            } else {
+                img.setAttribute('src', 'image/Fotoidoso M.jpg');
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src','image/Fotobb F.jpg');
+            } else if (idade < 21) {
+                img.setAttribute('src', 'image/Fotojovem F.jpg');
+            } else if (idade < 50) {
+                img.setAttribute('src', 'image/Fotoadulto F.jpg');
+            } else  {
+                img.setAttribute('src', 'image/Fotoidoso F.jpg');
+            }
+        }
+        res.style.textAlign = 'center0'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.innerHTML = `Idade calculada: ${idade}` 
+        res.appendChild(img)
+    } 
+}
--- Curso JS/Execercícios/script02/scripts.js
+++ Curso JS/Execercícios/script02/scripts.js
@@ -1,14 +1,27 @@
 function carregar() {
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    var msg = document.getElementById('msg');
+    var img = document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    var minutos = data.getMinutes();
+    var segundos = data.getSeconds();
+
+    minutos = minutos < 10 ? '0' + minutos : minutos;
+    segundos = segundos < 10 ? '0' + segundos : segundos;
+
+    msg.innerHTML = `Agora são ${hora}:${minutos}.`;
     if (hora >= 0 && hora < 12) {
-        img.src = 'image/Fotomanhã.jpg'
+        img.src = 'image/Fotomanhã.jpg';
+        document.body.style.background = '#A6B0BB';
     } else if (hora >= 12 && hora < 18) {
-        img.src = 'image/Fototarde.jpg'
+        img.src = 'image/Fototarde.jpg';
+        document.body.style.background = '#F3BD88';
     } else {
-        img.src = 'image/Fotonoite.jpg'
+        img.src = 'image/Fotonoite.jpg';
+        document.body.style.background = '#0b3f44';
     }
 }
+
+  carregar();
+
+  setInterval(carregar, 1000);
