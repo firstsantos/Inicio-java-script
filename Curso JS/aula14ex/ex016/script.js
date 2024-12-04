aa8b008688eadc3fa1e9c30c84f702a22bfa6c01
Artur Moreira dos Santos <arturmoreira56dossantos@gmail.com>
2024-12-04 14:37:20 -0300

    todos

--- Curso JS/aula14ex/ex016/script.js
+++ Curso JS/aula14ex/ex016/script.js
@@ -1,11 +1,26 @@
 function contar() {
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
-    let contagem = document.getElementById('txtp')
+    let passos = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    if (ini.value.length == 0 || fim.value.length == 0 || contagem.value.length == 0 ) {
+    if (ini.value.length == 0 || fim.value.length == 0 || passos.value.length == 0 ) {
         window.alert('[ERRO] Faltam dados!')
     } else {
-        alert('Ok!')
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passos.value)
+        if (i < f) {
+            //CONTAGEM CRESCENTE
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{27A1} `
+            }
+        } else {
+            //CONTAGEM REGRESSIVA
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{27A1}`
+            }
+        }
     }
 }
